6688f855526f4ba4bc2607264304c6ef3a938115
Paweł Malinowski <p@malinowski.pm>
2024-07-24 17:32:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# `effect_update_depth_exceeded` error
+
+```shell
+git clone git@github.com:malinowskip/carbon-multiselect-svelte-5-error.git &&
+cd carbon-multiselect-svelte-5-error &&
+pnpm install &&
+pnpm dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import "carbon-components-svelte/css/g100.css";
+  import { MultiSelect } from "carbon-components-svelte";
+</script>
+
+<section>
+  <p>
+    The MultiSelect component below causes the following error to be printed to
+    the console: <strong
+      >Uncaught (in promise) Svelte error: effect_update_depth_exceeded</strong
+    >
+  </p>
+  <hr />
+  <MultiSelect titleText="Test" label="Test" items={[{ id: 1, text: "Foo" }]} />
+</section>
+
+<style>
+  section {
+    max-width: 700px;
+    padding: 1rem;
+  }
+</style>
